739560222350c3e3fa5151daf669e6c9951426ed
Swagatk123 <125346454+Swagatk123@users.noreply.github.com>
2023-02-14 13:30:16 +0530

    added weather project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export  const environment = {
+    production:true,
+    
+    weatherApiBaseUrl: "https://open-weather13.p.rapidapi.com/city/landon",
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: 'dc34152093mshd14ce9b27432264p1317cajsn4783967b0b70'
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export  const environment = {
+    production:false,
+    weatherApiBaseUrl: "https://open-weather13.p.rapidapi.com/city/landon",
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: 'dc34152093mshd14ce9b27432264p1317cajsn4783967b0b70'
+
+    
+
+}
